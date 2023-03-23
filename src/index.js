5c17b0b6f1d2193f65e2625d6a0ff3fcad156daf
rosewere <studentconsult25@gmail.com>
2023-03-24 00:18:00 +0300

    Moved my weather-app html, css & js files to github

---
+++ src/index.js
@@ -0,0 +1,49 @@
+let now = new Date();
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let currentTime = `${day} ${hour}:${minutes} `;
+let timeElement = document.querySelector("#time");
+timeElement.innerHTML = `${currentTime}`;
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchTemp(event) {
+  event.preventDefault();
+  let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function searchLocation(position) {
+  let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchTemp);
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getLocation);
