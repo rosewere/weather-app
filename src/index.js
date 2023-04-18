6d5100c990cd2dd4f41d978966b50524e604cddf
rosewere <studentconsult25@gmail.com>
2023-04-18 21:26:07 +0300

    added forecast api

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ function formatDate(timestamp) {
   }
   return `${day} ${hour}: ${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
 
@@ -40,6 +41,13 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7784a4cd4aa2e0c25ead7bd96d585b8a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -60,6 +68,7 @@ function displayWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
@@ -110,4 +119,3 @@ let celciusLink = document.querySelector("#c-link");
 celciusLink.addEventListener("click", convertToCelcious);
 
 search("Nairobi");
-displayForecast();
